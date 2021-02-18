85b1d847ad46f0fe65e582bba30ce126148e72c9
neel685 <neel.mandal@gmail.com>
2021-02-18 07:43:04 +0000

    sdm845-common : Clean up lineage stuff and nuke livedisplay completely

--- sdm845.mk
+++ sdm845.mk
@@ -22,7 +22,6 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -201,10 +200,6 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm845
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_PACKAGES += \
